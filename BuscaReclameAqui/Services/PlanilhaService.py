d47041fa9dd474ef8d5858486ac1fe3b0d7e23ab
Andre <andreaugustosilvacarvalho@gmail.com>
2025-02-19 15:54:41 -0300

    feat:planilhaOk

--- BuscaReclameAqui/Services/PlanilhaService.py
+++ BuscaReclameAqui/Services/PlanilhaService.py
@@ -1,17 +1,21 @@
 import pandas as pd
 
+def tratar_percentual(valor):
+    return valor if valor != "" else "não informado"
+
 def escrevendoPlanilha(lojas):
-        dados = [{
+    dados = [{
         "Nome": loja.nome,
         "Nota": loja.nota,
         "Link": loja.href,
-        "Percentual Voltariam": loja.percentual_voltariam,
-        "Índice de Solução": loja.percentual_indice_solucao,
-        "Percentual Respondido": loja.percentual_respondido,
+        "Percentual Voltariam": tratar_percentual(loja.percentual_voltariam),
+        "Índice de Solução": tratar_percentual(loja.percentual_indice_solucao),
+        "Percentual Respondido": tratar_percentual(loja.percentual_respondido),
         "Número de Solicitações": loja.numero_solicitacoes
     } for loja in lojas]
-        
-        df = pd.DataFrame(dados)
-        nome_arquivo = "lojas.xlsx"
-        df.to_excel(nome_arquivo, index=False)
-        return nome_arquivo
+    
+    df = pd.DataFrame(dados)
+    nome_arquivo = "lojas.xlsx"
+    df.to_excel(nome_arquivo, index=False)
+    return nome_arquivo
+    
